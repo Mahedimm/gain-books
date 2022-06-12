de0c8780e6503e23ed75f5201079e5f56245a39a
Mahedimm <mahedi15-12649@diu.edu.bd>
2022-06-12 21:43:02 +0600

    details Page

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,6 +1,43 @@
 import React from 'react'
 import moment from 'moment';
 export default function PostDetail({post}) {
+    const getContentFragment = (index, text, obj, type) => {
+        let modifiedText = text;
+    
+        if (obj) {
+          if (obj.bold) {
+            modifiedText = (<b key={index}>{text}</b>);
+          }
+    
+          if (obj.italic) {
+            modifiedText = (<em key={index}>{text}</em>);
+          }
+    
+          if (obj.underline) {
+            modifiedText = (<u key={index}>{text}</u>);
+          }
+        }
+        switch (type) {
+            case 'heading-three':
+              return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+            case 'paragraph':
+              return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+            case 'heading-four':
+              return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+            case 'image':
+              return (
+                <img
+                  key={index}
+                  alt={obj.title}
+                  height={obj.height}
+                  width={obj.width}
+                  src={obj.src}
+                />
+              );
+            default:
+              return modifiedText;
+          }
+    }
   return (
     <div className="bg-gray-700 shadow-lg rounded-lg  pb-12 mb-8">
         <div className="relative overflow-hidden shadow-md mb-6">
@@ -22,6 +59,12 @@ export default function PostDetail({post}) {
                         </span>
                     </div>
             </div>
+            <h1 className="mb-8 text-3xl font-semibold text-center border-4 p-2 border-gray-500 rounded-md text-pink-500">{post.title}</h1>
+            {post.content.raw.children.map((typeObj, index) => {
+            const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
+
+            return getContentFragment(index, children, typeObj, typeObj.type);
+          })}
         </div>
     </div>
   )
